d8fd38a90e47b479ed873d6a05fd9fc21966914f
J-M <jean-michel.perraud@csiro.au>
2024-10-14 12:57:03 +1100

    WIP python bindings tests prior to rename a ile

--- tests/python_test_bindings/tests/testapi.py
+++ tests/python_test_bindings/tests/testapi.py
@@ -1,10 +1,9 @@
 import os
-import numpy as np
-import datetime as dt
 import sys
-from datetime import datetime
 
-pkg_dir = os.path.join(os.path.dirname(__file__),'..')
+import testmoirai.wrap.testmoirai_wrap_generated as twg
+
+pkg_dir = os.path.join(os.path.dirname(__file__), "..")
 
 sys.path.append(pkg_dir)
 
@@ -13,30 +12,30 @@ from testmoirai.classes import *
 # Note that these tests are very basic from a python interop perspective.
 # more elaborate ones are in the refcount package
 
+
 def test_basic_obj_lifetime():
     import gc
+
     cat = Cat.new()
     dog = Dog.new()
 
     cn = twg.get_name_py(cat)
     dn = twg.get_name_py(dog)
-    assert(cn == "cat")
-    assert(dn == "dog")
-    del(cat)
-    del(dog)
+    assert cn == "cat"
+    assert dn == "dog"
+    del cat
+    del dog
     gc.collect()
 
 
 def test_reference_count():
     import gc
+
     cat = Cat.new()
-    assert(twg.reference_count_py(cat) == 1)
+    assert twg.reference_count_py(cat) == 1
     mamal = twg.create_mamal_ptr_py(cat)
-    assert(twg.reference_count_py(cat) == 2)
-    del(cat)
+    assert twg.reference_count_py(cat) == 2
+    cat = None
     gc.collect()
-    assert(twg.reference_count_py(cat) == 1)
+    assert twg.reference_count_py(mamal) == 1
     # del(mamal)
-
-
-
